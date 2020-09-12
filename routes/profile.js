b9dd66922a773993d4031eb3ddf077eda9a47329
SidVermaS <sidvermas1234@gmail.com>
2020-09-12 16:51:41 +0530

    Add

--- routes/profile.js
+++ routes/profile.js
@@ -85,21 +85,21 @@ router.get('/following', async (req, res)=> {
         return res.status(500).json({ message: 'Failed to fetch the following', error: err })
     }
 })
-router.patch('/', async (req, res)=> {
+router.patch('/', async (req, res)=>{
+	const reqBody=req.body
     try {
-        const reqBody=req.body
-        const list=List({
-            _id: mongoose.Types.ObjectId(reqBody._id),
-            pinned: reqBody.pinned
-        })
-        const result=await List.updateOne({ _id: list._id }, { $set: list } )
-        if(result['nModified']>0)  {
-            return res.status(200).json({ message: 'Successfully changed the pin', result: result })
+        
+
+        const result=await List.deleteOne({ _id: reqBody._id })
+		
+        console.log('~~ del: ',result)
+        if(result)  {
+            return res.status(200).json({ message: 'Successfully deleted the profile', })
         }   else    {
-            return res.status(400).json({ message: 'Failed to change the pin' })
+            return res.status(400).json({ message: 'Failed to delete the profile' })
         }
     }   catch(err)  {
-        return res.status(500).json({ message: 'Failed to change the pin', error: err })
+        return res.status(500).json({ message: `Failed to ${reqBody} the profile`, error: err })
     }
 })
 
